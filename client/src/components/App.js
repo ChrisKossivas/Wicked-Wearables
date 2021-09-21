cdb8cea7a9030e3a478510eed357b01d04f1fbc8
“MigrantCaravan” <m.ospina.bedoya@gmail.com>
2021-09-21 13:42:08 -0400

    update item details

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage/index";
+import ItemDetails from "./ItemDetails/index";
 
 function App() {
   // const [bacon, setBacon] = useState(null);
@@ -17,9 +19,12 @@ function App() {
       <Main>
         <Switch>
           <Route exact path="/">
-            App
+            <HomePage></HomePage>
           </Route>
           <Route exact path="/cart"></Route>
+          <Route exact path="/itemDetail">
+            <ItemDetails></ItemDetails>
+          </Route>
           <Route exact path="/error"></Route>
         </Switch>
       </Main>
@@ -33,5 +38,10 @@ const Main = styled.div`
   /* background: var(--color-orange); */
   display: flex;
   flex-direction: column;
-  height: calc(100vh - 110px);
+  /* height: 100%; */
+  height: 100vh;
+  width: 100%;
+  background-color: #316b83;
+  background-image: url("https://www.transparenttextures.com/patterns/cubes.png");
+  /* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
 `;
