19a111f841615572287c46bbe75828c8d1872493
Dimitri Creticos <dimcreticos@gmail.com>
2021-09-22 10:59:09 -0400

    cartModal Opens and Closes

--- client/src/components/Cart/CartModal.js
+++ client/src/components/Cart/CartModal.js
@@ -1,1 +1,36 @@
 // Don't forget to comment
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Cart = ({ fetchCartRequest, productInCart, setIsCartOpen }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userData");
+    if (fetchCartRequest) {
+      fetchCartRequest(userId);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <button onClick={() => setIsCartOpen(false)}>X</button>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: whitesmoke;
+  position: absolute;
+  top: 80px;
+  right: 0;
+  height: 100vh;
+  width: 400px;
+`;
+
+export default Cart;
+
+//remember to add styling.
